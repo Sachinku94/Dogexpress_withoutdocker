40f3945af86300f395e78772d026be6537924dc5
Sachinku94 <140709136+Sachinku94@users.noreply.github.com>
2025-01-09 11:57:47 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,20 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from Config.config_reader import read_config
+from webdriver_manager.chrome import ChromeDriverManage
+
 
 @pytest.fixture(scope="class")
 def setup(request):
     # Read base URL from config
+    CHROMEDRIVER_VERSION = "114.0.5735.90"
+    driver = webdriver.Chrome(
+        ChromeDriverManager(version=CHROMEDRIVER_VERSION).install()
+    )
     base_url = read_config("URL", "base_url")
-    driver=webdriver.Chrome()
-    
+    driver = webdriver.Chrome()
+
     driver.get(base_url)
-    
+
     # Attach driver to the test class
     request.cls.driver = driver
-    
+
     # Yield to run tests
     yield
-    
+
     # Quit the driver after tests complete
     driver.quit()

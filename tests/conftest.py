e5f72a2b558d780d31d72f0221faae99e5dfc5ef
Sachinku94 <sachinku94@gmail.com>
2025-01-12 16:42:44 +0530

    modified

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from Config.config_reader import read_config
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="class")
 def setup(request):
@@ -12,9 +13,12 @@ def setup(request):
     CHROMEDRIVER_VERSION = "131.0.6778.205"
     path =ChromeDriverManager(driver_version=CHROMEDRIVER_VERSION).install()
     base_url = read_config("URL", "base_url")
-    driver=webdriver.Chrome(path)
+    chrome_options=Options
+    service=Service(path)
+    driver=webdriver.Chrome(service=service,options=chrome_options)
 
     driver.get(base_url)
+
 
     # Attach driver to the test class
     request.cls.driver = driver
